eb2602f452e2bd80bbc1ed6f66d4a8a9192b9b56
Akash Askoolum <akash1810@gmail.com>
2020-09-11 22:55:31 +0100

    specify types of generic functions

--- src/types/argo/data-entity.ts
+++ src/types/argo/data-entity.ts
@@ -1,7 +1,12 @@
 import * as t from "io-ts";
 import URLFromString from "~types/io-ts/URLFromString";
+import { Mixed, Type, TypeC } from "io-ts";
 
-const DataEntity = <C extends t.Mixed>(codec: C) =>
+const DataEntity: <C extends Mixed>(codec: C) => TypeC<{ data: C; uri: Type<URL, string, unknown> }> = <
+  C extends t.Mixed
+>(
+  codec: C
+) =>
   t.type(
     {
       uri: URLFromString,
--- src/types/argo/entity.ts
+++ src/types/argo/entity.ts
@@ -2,8 +2,21 @@ import * as t from "io-ts";
 import URLFromString from "~types/io-ts/URLFromString";
 import { ArgoAction } from "./action";
 import { ArgoLink } from "./link";
+import { IntersectionC, KeyofC, Mixed, PartialC, ReadonlyArrayC, StringC, Type, TypeC } from "io-ts";
 
-const ArgoEntity = <C extends t.Mixed>(codec: C) =>
+const ArgoEntity: <C extends Mixed>(
+  codec: C
+) => IntersectionC<
+  [
+    TypeC<{ data: C; url: Type<URL, string, unknown> }>,
+    PartialC<{
+      links: ReadonlyArrayC<TypeC<{ rel: StringC; href: Type<URL, string, unknown> }>>;
+      actions: ReadonlyArrayC<
+        TypeC<{ method: KeyofC<{ [p: string]: null }>; name: StringC; href: Type<URL, string, unknown> }>
+      >;
+    }>
+  ]
+> = <C extends t.Mixed>(codec: C) =>
   t.intersection([
     t.type({
       url: URLFromString,
