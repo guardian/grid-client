353abe5ebea016754884bbd01e1d18eb2140145f
Akash Askoolum <akash1810@gmail.com>
2020-09-14 22:15:49 +0100

    work around collection response issue

--- src/types/collection/collection.ts
+++ src/types/collection/collection.ts
@@ -1,6 +1,10 @@
 import * as t from "io-ts";
 import { ActionData } from "./action-data";
 
+// TODO correct this after https://github.com/guardian/grid/issues/3005 is resolved
+const BrokenCollection = t.type({});
+type BrokenCollection = t.TypeOf<typeof BrokenCollection>;
+
 const Collection = t.type({
   path: t.array(t.string),
   pathId: t.string,
@@ -10,4 +14,4 @@ const Collection = t.type({
 
 type Collection = t.TypeOf<typeof Collection>;
 
-export { Collection };
+export { Collection, BrokenCollection };
