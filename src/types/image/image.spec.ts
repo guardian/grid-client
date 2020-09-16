90d014aca3f0e6b20ccf6b8bbf3580113e80eea7
Akash Askoolum <akash1810@gmail.com>
2020-09-16 14:08:52 +0100

    an example usage of the generic reporter

--- src/types/image/image.spec.ts
+++ src/types/image/image.spec.ts
@@ -1,8 +1,21 @@
-import { isRight } from "fp-ts/Either";
+import { isLeft, isRight } from "fp-ts/Either";
 import { GridImage } from "./image";
 import { image } from "./__fixtures__/image";
+import { Reporter } from "../../utils";
 
 test("decoding standard image response", () => {
   const parsed = GridImage.decode(image);
   expect(isRight(parsed)).toBe(true);
+});
+
+test("decoding invalid input", () => {
+  const data = {
+    foo: "bar",
+  };
+
+  const parsed = GridImage.decode(data);
+
+  Reporter.default.log(parsed);
+
+  expect(isLeft(parsed)).toBe(true);
 });
